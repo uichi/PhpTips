23a3ef3934d0ec808d29778d733a972d462d27b7
Uichi <ykonishi@protonmail.com>
2018-03-30 23:56:35 +0900

    Add the output type.

---
+++ output_type.php
@@ -0,0 +1,15 @@
+<?php
+$data = array(
+    "A" => "Apple",
+    "B" => "Banana",
+    "C" => "Cherry"
+);
+
+echo "---print_r---\n";
+print_r($data);
+
+echo "---var_dump---\n";
+var_dump($data);
+
+echo "---var_export---\n";
+var_export($data);
